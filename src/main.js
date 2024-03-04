c1dfdc40a522bcac15c564180475f548ccae3b2f
Evan <littlenoob107@gmail.com>
2024-03-04 14:30:02 -0700

    uodate

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ let cookCost = document.getElementById('cookUpgCost');
 
 let cookUpgrades = document.getElementById('cookUpgCount');
 
+let blocker = document.getElementById("blocker");
+let patrickUnlock = document.getElementById("patrickUnlock");
+let buyPatrick = document.getElementById("buyPatrick");
+let patrickInfo = document.getElementById("patrickInfo")
+let patrickWorkerCount = document.getElementById("PatrickWorkerCount")
+let PatrickCashPerSecond = document.getElementById("PatrickCashPerSecond")
+let patrickCost = 50
+let PatrickEffectiveness = 5;
+
 // if cookbtn is cliecked, add 1 money;
 cookbtn.addEventListener('click', function(){
     money.innerHTML = parseInt(money.innerHTML) + 1 * (parseInt(cookUpgrades.innerHTML) + 1);
@@ -21,8 +30,41 @@ cookUpgrade.addEventListener('click', function(){
             money.innerHTML = parseInt(money.innerHTML) - 10 * (parseInt(cookUpgrades.innerHTML) + 1);
             cookUpgrades.innerHTML = parseInt(cookUpgrades.innerHTML) + 1;
             cookCost.innerHTML = 10 * (parseInt(cookUpgrades.innerHTML) + 1);
-
         }
 
     }
 });
+
+blocker.addEventListener('click', startPatrickFarm);
+
+
+function payForPatrick() {
+    setInterval(function () {
+        money.innerHTML = parseInt(money.innerHTML) + parseInt(PatrickCashPerSecond.innerHTML)
+    }, 1000);
+}
+
+function buyPatricks() {
+    if (parseInt(money.innerHTML) >= patrickCost) {
+        console.log("patrick buy");
+        money.innerHTML = parseInt(money.innerHTML) - patrickCost;
+        patrickWorkerCount.innerHTML = parseInt(patrickWorkerCount.innerHTML) + 1;
+        PatrickCashPerSecond.innerHTML = parseInt(PatrickCashPerSecond.innerHTML) + PatrickEffectiveness;
+    }
+}
+
+function startPatrickFarm() {
+    if (parseInt(money.innerHTML) >= 500) {
+        console.log(parseInt(money.innerHTML));
+        blocker.classList.remove("opacity-10");
+        patrickUnlock.classList.remove("visible");
+        patrickUnlock.classList.add("hidden");
+        buyPatrick.classList.remove("hidden");
+        patrickInfo.classList.remove("hidden");
+        money.innerHTML = parseInt(money.innerHTML) - 500;
+        console.log(money.innerHTML);
+        blocker.removeEventListener('click', startPatrickFarm);
+        buyPatrick.addEventListener('click', buyPatricks)
+        payForPatrick()
+    };
+};
